a340323f1ab3a5884cdeb8ec953472f68a320a38
Magesh-2005 <hackmagesh1@gmail.com>
2026-01-08 15:03:09 +0530

    Admin Service

---
+++ src/main/java/com/tnsif/adminservice/ShoppingMallManagementApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallManagementApplication.class, args);
+		System.out.println(" Shopping Mall Management Backend Started Successfully!");
+	}
+
+}
---
+++ src/main/java/com/tnsif/adminservice/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.tnsif.adminservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.tnsif.adminservice.entity.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByEmail(String email);
+}
